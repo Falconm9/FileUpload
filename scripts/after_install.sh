3c8b577824ef877a352772c8b31f88356a594e36
unknown <joselfalconml@hotmail.com>
2022-10-31 17:00:00 -0600

    add pm2

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,3 +2,4 @@
 cd /home/ec2-user/serverReact
 npm install
 npm install --save react react-dom react-scripts react-particles-js nodemon
+npm install pm2 -g
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/serverReact
 npm start
+pm2 start npm --name "malware-challenger" -- start
+pm2 startup
+pm2 save
+pm2 restart all
